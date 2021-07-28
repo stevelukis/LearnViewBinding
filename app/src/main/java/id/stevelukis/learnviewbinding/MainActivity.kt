62193e20428972e5152725fc7507ed42a68a0501
Steve Lukis <stevelukis88@gmail.com>
2021-07-28 13:58:15 +0800

    Initial commit

---
+++ app/src/main/java/id/stevelukis/learnviewbinding/MainActivity.kt
@@ -0,0 +1,20 @@
+package id.stevelukis.learnviewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            textView.setText(R.string.hello)
+        }
+    }
+}
