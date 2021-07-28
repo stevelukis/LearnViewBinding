f0b5ac7c5d66fb5560d6281da602618b3c8e5db0
Steve Lukis <stevelukis88@gmail.com>
2021-07-28 14:23:37 +0800

    Added view binding inside MainActivity.kt

--- app/src/main/java/id/stevelukis/learnviewbinding/MainActivity.kt
+++ app/src/main/java/id/stevelukis/learnviewbinding/MainActivity.kt
@@ -4,17 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import id.stevelukis.learnviewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
-
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            textView.setText(R.string.hello)
-        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
